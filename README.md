f980220ea88cd0b02f0bb455d6372a59e9baa121
Nevyn Bengtsson <nevyn@alloverse.com>
2021-06-16 00:47:19 +0200

    add a sweet crt tv

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ To start the app and connect it to an Alloplace for testing, run
 ```
 ./allo/assist run alloplace://nevyn.places.alloverse.com
 ```
+
+## License
+
+### Flynncade is MIT licensed
+
+### Flynncade uses these licenses
+
+"Magnavox 19" CRT TV - RR1938 W122" (https://skfb.ly/o6xw8) by amhyde is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).
--- lua/main.lua
+++ lua/main.lua
@@ -9,19 +9,26 @@ local app = App(client)
 
 assets = {
     quit = ui.Asset.File("images/quit.png"),
+    crt = ui.Asset.File("models/magnavox.glb"),
 }
 app.assetManager:add(assets)
 
-local mainView = RetroView(Bounds(0, 1.5, -2,   2, 2, 0.1))
-app:scheduleAction(1/mainView:getFps(), true, function()
-    mainView:poll()
+local main = ui.View(Bounds(0, 1.5, -2,   2, 2, 0.1))
+
+local tv = main:addSubview(ui.ModelView(Bounds(-1.32,0,0,  1,1,1):rotate(-3.14/2, 0,1,0), assets.crt))
+
+local background = main:addSubview(ui.Surface(Bounds(0, 0.05, -0.01,   0.80, 0.50, 0.01)))
+background:setColor({0,0,0,1})
+local emulator = main:addSubview(RetroView(Bounds(0,0.05,0,   0.65, 0.50, 0.01)))
+app:scheduleAction(1/emulator:getFps(), true, function()
+    emulator:poll()
 end)
 
 app:scheduleAction(5.0, true, function() 
     print("Network stats", app.client.client:get_stats())
 end)
 
-app.mainView = mainView
+app.mainView = main
 
 app:connect()
 app:run()
