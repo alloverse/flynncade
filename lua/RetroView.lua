f735a4c5301f109f75f08a592dcf835d1bac995b
Nevyn Bengtsson <nevyn@alloverse.com>
2021-06-15 11:13:45 +0200

    4fps is unplayable

--- lua/RetroView.lua
+++ lua/RetroView.lua
@@ -160,7 +160,7 @@ function RetroView:_video_refresh(data, width, height, pitch)
     end
     if self.frame_id == nil then self.frame_id = 0 end
     self.frame_id = self.frame_id + 1
-    if self.frame_id % 20 ~= 0 then return end
+    if self.frame_id % 10 ~= 0 then return end
 
     pitch = tonumber(pitch)
 
