a01399b60100e55b1119ff8d2825c96c5b4d3f8c
Tobias Kask <tobias.kask@gmail.com>
2022-01-18 01:45:00 +0100

    minor tweaks & cleanup

--- lua/GameBrowser.lua
+++ lua/GameBrowser.lua
@@ -98,11 +98,11 @@ function GameBrowser:listConsoles()
   local depth = 0
 
   -- Create a "page"; the surface on which the menu items will be drawn.
-  local page = self:addSubview(ui.Surface(ui.Bounds(0,0,0, 0, 0, 0):move(depth/10, -depth/10, depth/10)))
+  local page = self:addSubview(ui.Surface(ui.Bounds(0,0,0, 0, 0, 0):move(depth/60, -depth/60, depth/60)))
   page:setColor({0, 0, 1, 0.9})
 
   -- Move the mainView up-and-back so that the newly created page always remains on "z=0"
-  self.bounds:move(-depth/10, depth/10, -depth/10)
+  self.bounds:move(-depth/60, depth/60, -depth/60)
   self:markAsDirty("transform")
 
   -- Look through the console folders
@@ -111,7 +111,7 @@ function GameBrowser:listConsoles()
   for gamePath in p:lines() do
     print("The Game Browser found lines: "..gamePath)
 
-    -- TODO: substring magic to cut out the path from the label, just show the ultimate file- or folder name.
+    -- Quick hack to get the console's name from the path
     local consoleName = string.sub(gamePath, 6)
 
     -- Create a menu item with bounds relative to its parent-to-be page
@@ -119,15 +119,12 @@ function GameBrowser:listConsoles()
     menuItem:setColor({1, 1, 1, 1})
 
     -- Create a Label inside said menu item
-    local menuItemLabel = Label{bounds=Bounds(MENU_ITEM_PADDING, 0, 0.01, MENU_ITEM_WIDTH-(MENU_ITEM_PADDING*2), MENU_ITEM_HEIGHT-(MENU_ITEM_PADDING*4), 0.01), color={0.15,0.15,0.15,1}, text=consoleName, halign="left"}
+    local menuItemLabel = Label{bounds=Bounds(MENU_ITEM_PADDING, 0, 0.001, MENU_ITEM_WIDTH-(MENU_ITEM_PADDING*2), MENU_ITEM_HEIGHT-(MENU_ITEM_PADDING*4), 0.001), color={0.15,0.15,0.15,1}, text=consoleName, halign="left"}
     menuItem:addSubview(menuItemLabel)
 
     -- Make the menuItem interactive
     menuItem:setPointable(true)
     menuItem.onTouchUp = function(pointer)
-      print("==================")
-      print("Menu item clicked ")
-      print("==================")
       self:listGames(gamePath)
     end
 
@@ -138,19 +135,6 @@ function GameBrowser:listConsoles()
     menuItem.onPointerExited = function(pointer)
       menuItem:setColor({1, 1, 1, 1})
     end
-    
-    -- Why's this animation crashing? Also, is it possible to animate opacity?
-    -- Needs to be in doWhenAwake()
-    -- menuItem:addPropertyAnimation(ui.PropertyAnimation{
-    --   path= "transform.matrix.translation.x",
-    --   start_at = 0,
-    --   from= -0.2,
-    --   to=  0,
-    --   duration = 2.0,
-    --   repeats= false,
-    --   autoreverses= false,
-    --   easing= "quadIn",
-    -- })
 
     -- Add the menuItem to the page
     page:addSubview(menuItem)
@@ -164,12 +148,12 @@ function GameBrowser:listGames(path)
   local depth = 1
   
   -- Create a "page"; the surface on which the menu items will be drawn.
-  local page = ui.Surface(ui.Bounds(0,0,0, 0, 0, 0):move(depth/10, -depth/10, depth/10))
+  local page = ui.Surface(ui.Bounds(0,0,0, 0, 0, 0):move(depth/60, -depth/60, depth/60))
   page:setColor({0, 0, 1, 0.9})
   self:addSubview(page)
 
   -- Move the mainView up-and-back so that the newly created page always remains on "z=0"
-  self.bounds:move(-depth/10, depth/10, -depth/10)
+  self.bounds:move(-depth/60, depth/60, -depth/60)
   self:markAsDirty("transform")
 
   -- Iterate through the folder
@@ -196,16 +180,12 @@ function GameBrowser:listGames(path)
       menuItem:setColor({1, 1, 1, 1})
 
       -- Create a Label inside said menu item
-      local menuItemLabel = Label{bounds=Bounds(MENU_ITEM_PADDING, 0, 0.01, MENU_ITEM_WIDTH-(MENU_ITEM_PADDING*2), MENU_ITEM_HEIGHT-(MENU_ITEM_PADDING*4), 0.01), color={0.15,0.15,0.15,1}, text=game.meta.gameName, halign="left"}
+      local menuItemLabel = Label{bounds=Bounds(MENU_ITEM_PADDING, 0, 0.001, MENU_ITEM_WIDTH-(MENU_ITEM_PADDING*2), MENU_ITEM_HEIGHT-(MENU_ITEM_PADDING*4), 0.001), color={0.15,0.15,0.15,1}, text=game.meta.gameName, halign="left"}
       menuItem:addSubview(menuItemLabel)
 
       -- Make the menuItem interactive
       menuItem:setPointable(true)
       menuItem.onTouchUp = function(pointer)
-        print("==================")
-        print("Menu item clicked ")
-        print("==================")
-
         self:showGame(game)
       end
 
@@ -217,18 +197,6 @@ function GameBrowser:listGames(path)
         menuItem:setColor({1, 1, 1, 1})
       end
       
-      -- Why's this animation crashing? Also, is it possible to animate opacity?
-      -- menuItem:addPropertyAnimation(ui.PropertyAnimation{
-      --   path= "transform.matrix.translation.x",
-      --   start_at = 0,
-      --   from= -0.2,
-      --   to=  0,
-      --   duration = 2.0,
-      --   repeats= false,
-      --   autoreverses= false,
-      --   easing= "quadIn",
-      -- })
-
       -- Add the menuItem to the page
       page:addSubview(menuItem)
 
@@ -244,16 +212,16 @@ function GameBrowser:showGame(game)
   local depth = 2
 
   -- Create a "page"; the surface on which the menu items will be drawn.
-  local page = ui.Surface(ui.Bounds(0,0,0, 1, 1, 0.01):move(depth/10, -depth/10, depth/10))
-  page:setColor({0.1, 0.1, 0.1, 1})
+  local page = ui.Surface(ui.Bounds(0, 0, 0.01, 1, MENU_ITEM_HEIGHT, 0.01):move(depth/60, -depth/60, depth/60))
+  page:setColor({1, 0, 1, 1})
   page:setTexture(game.meta.albumArt)
   self:addSubview(page)
 
   -- Move the mainView up-and-back so that the newly created page always remains on "z=0"
-  self.bounds:move(-depth/10, depth/10, -depth/10)
+  self.bounds:move(-depth/60, depth/60, -depth/60)
   self:markAsDirty("transform")
 
-  local playButton = ui.Button(ui.Bounds(0, 0, 0,  1, 0.1, 0.1))
+  local playButton = ui.Button(ui.Bounds(0, 0, 0, MENU_ITEM_WIDTH-MENU_ITEM_PADDING*2, 0.1, 0.1))
   playButton.label:setText("Play " .. game.meta.gameName)
 
   local gameInfo = Label{bounds=Bounds(0, 0, 0, 1.0, 0.05, 0.001), color={0.9,0.9,0.9,1}, text=game.meta.blurb, halign="left", wrap=true}
