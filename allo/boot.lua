8f43a1f4fe8126725cadc23381728f387792bb83
Patrik Sjöberg <voxxar@gmail.com>
2022-04-01 11:35:47 +0200

    boot loads allonet if needed

--- allo/boot.lua
+++ allo/boot.lua
@@ -27,8 +27,6 @@ elseif string.match(package.cpath, "dll") then
     dylibext = "dll"
 end
 
-package.cpath = package.cpath..";"..libDir.."/?."..dylibext
-
 package.path = package.path
     ..";"..srcDir.."/?.lua"
     ..";"..depsDir.."/alloui/lua/?.lua"
@@ -41,9 +39,13 @@ local libav_available, av = pcall(ffi.load, libDir .. "/liballonet_av."..dylibex
 if not libav_available then
     av = nil
     print("NOTE: liballonet_av not available, h264 cannot be used")
+
+    -- load liballonet
+    allonet = ffi.load(libDir .. "/liballonet."..dylibext, false)
 else
+    -- also loads allonet via weak linking
     print("liballonet_av loaded with libavcodec support")
-    ffi.load(libDir .. "/liballonet."..dylibext, true)
+    ffi.load(libDir .. "/liballonet."..dylibext, false)
     ffi.cdef [[
     void allo_libav_initialize(void);
     ]]
